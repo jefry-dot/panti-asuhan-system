bf0f50352d7403701041d93f3c192ee4a22b256c
adelyaharyani <adelyaharyani07@gmail.com>
2025-11-14 08:44:18 +0700

    fix dashboard berita

--- resources/views/public/berita/show.blade.php
+++ resources/views/public/berita/show.blade.php
@@ -6,7 +6,7 @@
     <article class="card shadow-sm p-4">
         <h2>{{ $berita->judul }}</h2>
         <img src="{{ asset('storage/' . $berita->gambar) }}" class="img-fluid my-3 rounded">
-        <p>{!! nl2br(e($berita->deskripsi)) !!}</p>
+        <p>{!! nl2br(e($berita->konten)) !!}</p>
         <a href="{{ route('public.berita') }}" class="btn btn-secondary mt-3">← Kembali ke daftar berita</a>
     </article>
 @endsection
